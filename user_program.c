c2ba694f2d84972cc3dd1b042eb2e66321b23f73
Milksisis <106484677+Milksisis@users.noreply.github.com>
2022-12-10 20:09:01 +0300

    Update user_program.c

--- user_program.c
+++ user_program.c
@@ -1,7 +1,3 @@
-//
-// Created by dinar on 04.12.2022.
-//
-
 #include "user_program.h"
 
 #define WR_DATA_TASK_STRUCT _IOW('a','a',int32_t*)
@@ -9,10 +5,10 @@
 #define RD_DATA _IOR('a','b',int32_t*)
 
 void take_data_from_user(char str[30], int32_t *pid){
-    printf("Enter the data to send ...\n");
-    printf("PID = ");
+    printf("Enter the data to send \n");
+    printf("Write your PID: ");
     scanf("%d",pid);
-    printf("\nEnter struct ");
+    printf("\nWrite your struct: ");
     scanf("%s",str);
     printf("%s",str);
 }
@@ -20,19 +16,19 @@ void take_data_from_user(char str[30], int32_t *pid){
 int main(){
     int fd;
     int32_t num;
-    char task_struct[] = "task_struct";// your struct name
-    char vfsmount[] = "vfsmount";// your struct name
+    char task_struct[] = "task_struct";
+    char vfsmount[] = "vfsmount";
     char str[30];
     char buf[256];
-    printf("\n IOCTL based Character device operation from user space...\n");
+    printf("\n IOCTL based Character device operation from user space \n");
     fd = open("/dev/my_device",O_RDWR);
 
     if(fd<0){
-        printf("Sorry, I can't open the device file...\n\n");
+        printf("The device file does not open \n\n");
         return 0;
     }
     take_data_from_user(str,&num);
-    printf("Writing value to the driver...\n\n");
+    printf("Writing value to the driver \n\n");
     if(strcmp((char *)&task_struct,str)==0) {
         ioctl(fd, WR_DATA_TASK_STRUCT, (int32_t * ) & num);
     }
@@ -40,14 +36,14 @@ int main(){
         ioctl(fd,WR_DATA_VFSMOUNT,(int32_t*)&num);
     }
     else{
-        printf("I don't know a struct %s\n\n", str);
+        printf("This structure is unknown to me %s\n\n", str);
         close(fd);
         return 0;
     }
     printf("Reading value from driver...\n\n");
     ioctl(fd,RD_DATA,(char*)&buf);
     printf("%s\n",buf);
-    printf("Closing driver...\n\n");
+    printf("Closing driver \n\n");
     close(fd);
     return 0;
 }
